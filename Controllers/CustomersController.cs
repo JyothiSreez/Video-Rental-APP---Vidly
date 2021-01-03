10cb0d54c13fcec8ff0f9e26d73a7c42e2868f59
JyothiSreez <38304110+JyothiSreez@users.noreply.github.com>
2021-01-02 17:18:58 -0700

    Add files via upload
    Vidly

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+using Vidly1.ViewModels;
+
+namespace Vidly1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+
+
+        public ActionResult Index()
+        {
+                
+            var customers = new List<Customers>
+
+            {
+
+                new Customers { Id = 1, Name = "John Smith" },
+                new Customers { Id = 2, Name = "Mary Williams" }
+            };
+
+
+            var viewModel = new DetailsCustomerViewModel
+            {
+                Customers = customers
+
+
+            };
+
+            return View(viewModel);
+        }
+
+
+
+        public ActionResult Details()
+        {
+
+
+            var customers = new List<Customers>
+            {
+
+            new Customers { Id = 1, Name = "John Smith" },
+            new Customers { Id = 2, Name = "Mary Williams" }
+
+           
+            };
+            
+
+            if (!customers.Any())
+                return HttpNotFound();
+
+            var viewModel = new DetailsCustomerViewModel
+        
+            {
+                Customers = customers
+
+            };
+            
+
+            return View(viewModel);
+
+
+
+    }
+
+}
+}
+
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+using Vidly1.ViewModels;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+
+            var movie = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+
+            };
+
+
+            var viewModel = new DetailsMovieViewModel
+            {
+                Movie = movie
+
+
+            };
+            return View(viewModel);
+
+        }
+
+
+        public ActionResult Details()
+        {
+
+
+            var movie = new List<Movie>
+            {
+
+            new Movie { Id = 1, Name = "Shrek" },
+            new Movie { Id = 2, Name = "Wall-e" }
+
+
+            };
+
+
+            if (!movie.Any())
+                return HttpNotFound();
+
+            var viewModel = new DetailsMovieViewModel
+
+            {
+                Movie = movie
+
+            };
+
+
+            return View(viewModel);
+
+
+        }
+
+
+
+    }
+}
